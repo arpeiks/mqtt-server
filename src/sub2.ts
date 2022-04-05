6a9a08a2b9a2c8270153a949c9a531cd9de3329a
arpeiks <arpeiks@gmail.com>
2022-04-05 12:36:11 +0100

    updated subscribers

--- src/sub2.ts
+++ src/sub2.ts
@@ -7,7 +7,7 @@ const client = mqtt.connect("mqtt://broker.hivemq.com", {
 
 client.on("connect", (pack) => {
   client.subscribe("random", { qos: 1 }); // quality of service > 1 [matches publisher]
-  console.log("SUBSCRIBER connected", pack);
+  console.log("SUBSCRIBER 2 connected", pack);
 });
 
 client.on("message", (topic, message, pack) => {
